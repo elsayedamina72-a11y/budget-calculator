ce117c5f331d18239340852e7c1efdcd5aef1337
elsayedamina72-a11y <elsayedamina72@gmail.com>
2025-10-20 17:18:57 +0300

    Add budget calculation and output logic

--- budget_calculator.py
+++ budget_calculator.py
@@ -1,1 +1,17 @@
 
+item1_price = 20
+item2_price = 30
+item3_price = 15
+budget = 50
+total_cost = item1_price + item2_price + item3_price
+
+print(f"Total cost: ${total_cost}")
+
+if total_cost <= budget:
+    print("Within budget!")
+    money_left = budget - total_cost
+    print(f"Money left: ${money_left}")
+else:
+    print("Over budget!")
+    money_needed = total_cost - budget
+    print(f"More money needed: ${money_needed}")
